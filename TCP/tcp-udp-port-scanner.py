d046dbfb2b1daf03c94c07f29ddaac595752e826
V11d <blahmous80@gmail.com>
2026-07-21 19:13:13 +0000

    Comment out UDP socket creation in port scanner
    Commented out the UDP socket creation line for clarity.

--- TCP/tcp-udp-port-scanner.py
+++ TCP/tcp-udp-port-scanner.py
@@ -5,6 +5,8 @@ ip = input("Enter IP: ")
 for port in range(1,  65535):
 
     scanner = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #tcp socket
+             #socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  #udp socket
+
     scanner.settimeout(1)
 
     try:
